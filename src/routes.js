970df4339921fa55b5430182f7e6f6f2e4156c43
annaclaraf <anisferraz@gmail.com>
2022-05-11 16:23:20 -0300

    auth route

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ const express = require('express')
 const routes = express.Router()
 
 const FuncionarioController = require('./controllers/FuncionarioController')
+const AuthController = require('./controllers/AuthController')
 
 routes.get('/funcionarios', FuncionarioController.index)
 routes.post('/funcionarios', FuncionarioController.create)
 routes.put('/funcionarios/:id', FuncionarioController.update)
 routes.delete('/funcionarios/:id', FuncionarioController.delete)
+
+routes.post('/login', AuthController.login)
 
 module.exports = routes 

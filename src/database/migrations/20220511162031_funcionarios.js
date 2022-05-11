ab4f9aa7b140e1cbdb7f90afad003cd1692abfd6
annaclaraf <anisferraz@gmail.com>
2022-05-11 15:03:40 -0300

    funcionarios migration

---
+++ src/database/migrations/20220511162031_funcionarios.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('funcionarios', table => {
+        table.string('CPF').notNullable().unique().primary()
+    
+        table.string('nome').notNullable()
+        table.string('email').notNullable().unique()
+        table.string('telefone').notNullable()
+        table.string('setor').notNullable()
+        
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now())
+      })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('funcionarios')
+};

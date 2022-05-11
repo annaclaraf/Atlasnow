0be09933b109891c9ab67ee85cb8256fbbd20997
annaclaraf <anisferraz@gmail.com>
2022-05-11 15:05:50 -0300

    add error handling

--- src/server.js
+++ src/server.js
@@ -7,4 +7,9 @@ app.use(express.json());
 
 app.use(routes)
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message}) 
+})
+
 app.listen(3000, () => console.log("Server rodando")); 

2b10279f6b5dec8be3379a333348b897c461044f
annaclaraf <anisferraz@gmail.com>
2022-05-19 13:43:04 -0300

    emissor routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const authMiddleware = require('./middlewares/auth')
 const routes = express.Router()
 
 const FuncionarioController = require('./controllers/FuncionarioController')
+const EmissorController = require('./controllers/EmissorController')
 const AuthController = require('./controllers/AuthController')
 
 routes.post('/login', AuthController.login)
@@ -15,5 +16,11 @@ routes.get('/funcionarios/:id', FuncionarioController.show)
 routes.post('/funcionarios', FuncionarioController.create)
 routes.put('/funcionarios/:id', FuncionarioController.update)
 routes.delete('/funcionarios/:id', FuncionarioController.delete)
+
+routes.get('/emissores', EmissorController.index)
+routes.get('/emissores/:id', EmissorController.show)
+routes.post('/emissores', EmissorController.create)
+routes.put('/emissores/:id', EmissorController.update)
+routes.delete('/emissores/:id', EmissorController.delete)
 
 module.exports = routes 
